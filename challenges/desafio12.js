696b34026d6663458f483d3518de69bea69589f9
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-08-16 17:42:33 -0300

    REQ12 - additing requirement content - [DONE]

---
+++ challenges/desafio12.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany({}, {
+  $push: {
+    valoresNutricionais: {
+      $each: [],
+      $sort: {
+        percentual: -1,
+      },
+    },
+  },
+});
+
+db.produtos.find({}, {
+  nome: true,
+  _id: false,
+  valoresNutricionais: true,
+});
