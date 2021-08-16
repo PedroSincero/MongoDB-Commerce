9e1053a755b4829cfb917dab06d194d5b7c82531
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-08-16 18:07:08 -0300

    REQ13 - additing requirement content - [DONE]

---
+++ challenges/desafio13.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: "sódio",
+      percentual: {
+        $gte: 40,
+      },
+    },
+  },
+}, {
+  $push: {
+    tags: "muito sódio",
+  },
+});
+
+db.produtos.find({}, {
+  nome: true,
+  _id: false,
+  tags: true,
+});
